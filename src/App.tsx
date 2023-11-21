9fbd27058ada2727a4f35c45418bc4d2334c741b
Kaan Sun <kaansun06@gmail.com>
2023-11-21 09:00:24 -0500

    Added tailwind, created the header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from 'react-bootstrap';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <Button> Hello</Button>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -24,6 +26,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
