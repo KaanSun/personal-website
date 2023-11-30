7181996f4658f9c90218922c6b5304f1ff7ddaef
Kaan Sun <kaansun06@gmail.com>
2023-11-30 01:05:25 -0500

    Added picture and description

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -21,13 +21,28 @@ function LandingPage() {
             <h3>Kaan Sun</h3>
             <div className="container ">
                 <div className='row justify-content-center'>
-                    <div className='col'>Computer Science Student with a strong interest in software development, artificial intelligence, and
-physics</div>
-                    <a className="col" href="https://github.com/KaanSun/personal-website">
-                    <img className="github-link landing" src="src/assets/github-white.png" alt="Github Repository of this Website" />
-                    </a>
+                    <div className='col'>
+                        <p> Computer Science Student with a strong interest in software development, artificial intelligence, and physics</p>
+                        <img className="profile-picture-big" src="src/assets/ks.png" alt="a picture of me" />
+                    </div>       
+                    <div className='col'>
+                        <p>Currently attending School of Computing at:</p>
+                        <a href="https://www.cs.queensu.ca/"><h1>Queen's University</h1></a>
+                        <br />
+                        <br />
+                        <p>GitHub account can be found <a href="https://github.com/KaanSun">here</a> or bottom right side of this page.</p>
+                        <br />
+                        <br />
+                        <p>Thank you for visiting!</p>
+                        <br />
+                        <br />
+                    </div>
+                    
                 </div>
             </div>
+                        <a className=" " href="https://github.com/KaanSun/">
+                        <img className="github-link" src="src/assets/github-white.png" alt="My Github Account" />
+                        </a>
            
 
             
