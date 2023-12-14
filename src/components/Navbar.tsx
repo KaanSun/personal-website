232b1726c801641b87c68511e11b74a147d4ff94
Kaan Sun <kaansun06@gmail.com>
2023-12-13 19:33:49 -0500

    Live v1.4

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,4 @@
 import Container from 'react-bootstrap/Container';
-import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
